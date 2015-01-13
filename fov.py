98450bd9bb3409ed627f29d9621802752ace1496
Michael Moore <mscottmoore16@gmail.com>
2015-01-13 23:24:50 +0800

    Optimized fov a bit - refresh takes about 1/5 the time it used to

--- fov.py
+++ fov.py
@@ -1,5 +1,6 @@
 import libtcodpy as libtcod
 from config import *
+import time
 import util
 
 class FOV:
@@ -12,6 +13,11 @@ class FOV:
     @property
     def thing(self):
         return self.creature.owner
+
+    def clear(self):
+        self.fov_map = [[0 for y in range(LEVEL_H)]
+                        for x in range(LEVEL_W)]
+
     def refresh(self):
         tcod_map = self.game.dungeon.tcod_map
         if self.thing is self.game.player:
@@ -23,18 +29,28 @@ class FOV:
                                      self.thing.x,
                                      self.thing.y,
                                      self.creature.perception*2)
-
-        for x in range(len(self.fov_map)):
-            for y in range(len(self.fov_map[0])):
-                fov_num = int(libtcod.map_is_in_fov(tcod_map,x,y))
-                if fov_num:
-                    if (x-self.thing.x==0 or
-                        y-self.thing.y==0 or
-                        abs(x-self.thing.x)-abs(y-self.thing.y)==0):
-                        fov_num += 1
-                    if util.distance(self.thing.x,self.thing.y,x,y)<=self.creature.perception:
-                        fov_num += 1
-                self.fov_map[x][y] = fov_num
+        if self.thing is self.game.player:
+            for x in range(len(self.fov_map)):
+                for y in range(len(self.fov_map[0])):
+                    self.fov_map[x][y]=int(libtcod.map_is_in_fov(tcod_map,
+                                                                 x,y))
+        else:
+            self.clear()
+            for x in range(self.thing.x-self.creature.perception*2,
+                           self.thing.x+self.creature.perception*2):
+                for y in range(self.thing.y-self.creature.perception*2,
+                               self.thing.y+self.creature.perception*2):
+                    fov_num = int(libtcod.map_is_in_fov(tcod_map,x,y))
+                    if fov_num:
+                        if (x-self.thing.x==0 or
+                            y-self.thing.y==0 or
+                            abs(x-self.thing.x)-abs(y-self.thing.y)==0):
+                            fov_num += 1
+                        if util.distance(self.thing.x,self.thing.y,x,y)<=self.creature.perception:
+                            fov_num += 1
+                    try:
+                        self.fov_map[x][y] = fov_num
+                    except IndexError: pass
     def can_see(self,x,y=None):
         if y != None:
             return self.fov_map[x][y]
